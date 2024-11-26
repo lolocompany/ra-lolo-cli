f1b081a541951fa4b65a31c7b713e3499f102b07
ex7r3me <siavash.ahmadpour@gmail.com>
2024-11-26 14:39:11 +0100

    add 'version' to exclude list

--- utils/excludeFields.js
+++ utils/excludeFields.js
@@ -1,7 +1,7 @@
 export const EXCLUDE_FIELDS = {
-    create: ["createdAt", "updatedAt", "createdBy", "updatedBy", "id"],
-    list: ["createdAt", "updatedAt", "createdBy", "updatedBy", "id"],
-    edit: ["createdAt", "updatedAt", "createdBy", "updatedBy", "id"],
-    show: ["createdBy", "updatedBy"],
-    filter: []
-  };
+  create: ["createdAt", "updatedAt", "createdBy", "updatedBy", "id", "version"],
+  list: ["createdAt", "updatedAt", "createdBy", "updatedBy", "id", "version"],
+  edit: ["createdAt", "updatedAt", "createdBy", "updatedBy", "id", "version"],
+  show: ["createdBy", "updatedBy", "version"],
+  filter: [],
+};
